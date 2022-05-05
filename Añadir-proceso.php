d5943fa8b37f697fb05b4a0d07228b7f89ae7f0b
SamaAxl <SamaAxl@protonmail.com>
2022-05-05 11:49:40 -0600

    Estilo página error

--- Añadir-proceso.php
+++ Añadir-proceso.php
@@ -1,6 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-include("conec.php");
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Farmacia</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <header class="header">
+    <nav class="navegacion">
+      <a href="index.html"><img class="logo" src="img/logo somos.png" alt="Somos Logo"></a>
+
+      <ul>
+        <li class="nav-item"><a href="index.html">Inicio</a></li>
+        <li class="nav-item"><a href="Clientes.php">Clientes</a></li>
+        <li class="nav-item"><a href="Medicamentos.php">Medicamentos</a></li>
+        <li class="nav-item"><a href="Movimientos.php">Movimientos</a></li>
+        <li class="nav-item"><a href="Proveedores.php">Proveedores</a></li>
+        <li class="nav-item"><a href="Facturas.php">Facturas</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main class="main">
+    <article class="container-table">
+      <?php
+
+    include("conec.php");
     $conexion=Conectarse();
    
     $id=$_POST["cvecte"];
@@ -14,13 +42,36 @@ include("conec.php");
 
     $resultado = mysqli_query( $conexion, $consulta );
    
-   if ($resultado==1) {
+    if ($resultado==1) {
         echo "Registro insertado exitosamente...<br>";
         header("Location: Clientes.php");
     }
-	else {	
-	    echo "Función fallida. Revisa la sintaxis y/o conexion al servidor.<br>";
+	  else {	
+	      echo "<h1 class='fallido'>Función fallida. Revisa la sintaxis y/o conexion al servidor.</h1>";
     }   
-  
-
 ?>
+      <div class="links">
+        <ul>
+          <li><a href="Clientes.php">Regresar</a></li>
+        </ul>
+      </div>
+    </article>
+  </main>
+  <footer class="footer">
+    <article class="footer-item">
+      <h1>Somos Software.</h1>
+    </article>
+    <article class="footer-item">
+      <p>Más sobre nosotros</p>
+      <ul>
+        <li><a href="">Sección 1</a></li>
+        <li><a href="">Sección 2</a></li>
+        <li><a href="">Sección 3</a></li>
+        <li><a href="">Sección 4</a></li>
+        <li><a href="">Sección 5</a></li>
+      </ul>
+    </article>
+  </footer>
+</body>
+
+</html>
--- Modificar-Proceso.php
+++ Modificar-Proceso.php
@@ -1,18 +1,69 @@
-<?php
-    include("conec.php");
-    $link=Conectarse();
+<!DOCTYPE html>
+<html lang="en">
 
-    $Sql="call ACTUALIZAR_CLIENTE('".$_POST["cvecte"]."','".$_POST["nombre"]."','".$_POST["curp"]."','".$_POST["direccion"]."','".$_POST["telefono"]."','".$_POST["correo"]."');";
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Farmacia</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
 
-    echo $Sql;
+<body>
+  <header class="header">
+    <nav class="navegacion">
+      <a href="index.html"><img class="logo" src="img/logo somos.png" alt="Somos Logo"></a>
 
-    $resultado=mysqli_query($link,$Sql);
+      <ul>
+        <li class="nav-item"><a href="index.html">Inicio</a></li>
+        <li class="nav-item"><a href="Clientes.php">Clientes</a></li>
+        <li class="nav-item"><a href="Medicamentos.php">Medicamentos</a></li>
+        <li class="nav-item"><a href="Movimientos.php">Movimientos</a></li>
+        <li class="nav-item"><a href="Proveedores.php">Proveedores</a></li>
+        <li class="nav-item"><a href="Facturas.php">Facturas</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main class="main">
+    <article class="container-table">
+      <?php
+        include("conec.php");
+        $link=Conectarse();
+
+        $Sql="call ACTUALIZAR_CLIENTE('".$_POST["cvecte"]."','".$_POST["nombre"]."','".$_POST["curp"]."','".$_POST["direccion"]."','".$_POST["telefono"]."','".$_POST["correo"]."');";
+
+        $resultado=mysqli_query($link,$Sql);
  
-    if ($resultado==1) {
-        echo "Registro modificado exitosamente...";
-        header("Location: Clientes.php");
-	}
-    else {
-		echo "Función fallida. Revisar sintaxis y/o conexion del servidor";
-	} 
+        if ($resultado==1) {
+            echo "Registro modificado exitosamente...";
+            header("Location: Clientes.php");
+        }
+        else {	
+            echo "<h1 class='fallido'>Función fallida. Revisa la sintaxis y/o conexion al servidor.</h1>";
+        } 
 ?>
+      <div class="links">
+        <ul>
+          <li><a href="Clientes.php">Regresar</a></li>
+        </ul>
+      </div>
+    </article>
+  </main>
+  <footer class="footer">
+    <article class="footer-item">
+      <h1>Somos Software.</h1>
+    </article>
+    <article class="footer-item">
+      <p>Más sobre nosotros</p>
+      <ul>
+        <li><a href="">Sección 1</a></li>
+        <li><a href="">Sección 2</a></li>
+        <li><a href="">Sección 3</a></li>
+        <li><a href="">Sección 4</a></li>
+        <li><a href="">Sección 5</a></li>
+      </ul>
+    </article>
+  </footer>
+</body>
+
+</html>
